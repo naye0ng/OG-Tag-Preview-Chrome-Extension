c4a47499fdda59cdcb1c5772cec1c8d53bb91d83
naye0ng <nayeong_e@naver.com>
2019-01-01 18:55:14 +0900

    해딩링크의 html elements 받아오기

--- popup.js
+++ popup.js
@@ -5,6 +5,8 @@
     popup.html에 og태그 출력
 */
 
+var xhr;
+
 // 현재 탭의 링크 받기 
 function getCurrentTabUrl(callback) {
     var queryInfo = {
@@ -24,16 +26,54 @@ function getCurrentTabUrl(callback) {
 function renderURL(statusText) {
     document.getElementById('urls').textContent = statusText;
 }
-  
+
+
+// 해딩 URL의 객체들 받아오기 
+function parse(html){
+    var el = document.createElement('div');
+    el.innerHTML = html;
+    
+    var meta = el.querySelectorAll('meta');
+    alert("성공 : "+meta);
+
+
+}
+
+
+function handleStateChange(){
+    if(xhr.readyState==4){
+        if(xhr.status==200){
+            parse(xhr.responseText);
+            //alert("성공 : "+xhr.responseText);
+        }
+        else{
+            alert("실패 : "+xhr.status);
+        }
+    }
+  }  
+ 
+function getURLDom(targetURL) {
+
+    xhr = new XMLHttpRequest();
+    xhr.onreadystatechange=handleStateChange;
+    xhr.open('GET',targetURL,true);
+    //xhr.setRequestHeader("content-Type","application/json;charset=utf-8");
+    //xhr.send(JSON.stringify(msg));
+    xhr.send();
+
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   
     // when click, get current page link
     var link = document.getElementById('getUrl');
   
     link.addEventListener('click', function() {
-      getCurrentTabUrl(function(url) {
-        renderURL(url);
-      });
+        getCurrentTabUrl(function(url) {
+            renderURL(url);
+            getURLDom(url);
+        });
     });
 
 });
